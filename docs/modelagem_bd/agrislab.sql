335bd292ccf40e5950f10a37c1caadeae7c9a826
404NotFound-ABP <fatecgirls207@gmail.com>
2025-10-23 22:35:30 -0300

    Esquema SQL refeito

--- docs/modelagem_bd/agrislab.sql
+++ docs/modelagem_bd/agrislab.sql
@@ -1,50 +1,44 @@
 CREATE TABLE IF NOT EXISTS "noticias" (
-	"id_noticias" serial NOT NULL UNIQUE,
-	"titulo" varchar(30) NOT NULL,
-	"subtitulo" varchar(100),
-	"data" timestamp with time zone NOT NULL,
+	"id_noticias" serial PRIMARY KEY,
+	"titulo" varchar(100) NOT NULL,
+	"subtitulo" varchar(255),
+	"data" timestamp with time zone NOT NULL DEFAULT NOW(),
 	"url_imagem" varchar(255) NOT NULL,
-	"texto" varchar(5000) NOT NULL,
-	"categoria" varchar(30) NOT NULL,
-	"destaque" boolean NOT NULL,
-	PRIMARY KEY ("id_noticias")
+	"texto" TEXT NOT NULL,
+	"id_categoria" integer NOT NULL,
+	"destaque" boolean NOT NULL DEFAULT false,
+	CONSTRAINT "fk_categoria_noticias" FOREIGN KEY ("id_categoria") REFERENCES "categoria_noticias"("id_categoria_noticias") ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS "publicacoes" (
-	"id_publicacoes" serial NOT NULL UNIQUE,
-	"titulo" varchar(50) NOT NULL,
+	"id_publicacoes" serial PRIMARY KEY,
+	"titulo" varchar(255) NOT NULL,
 	"autores" varchar(255) NOT NULL,
-	"data" timestamp with time zone NOT NULL,
+	"data" timestamp with time zone NOT NULL DEFAULT NOW(),
 	"url_imagem" varchar(255) NOT NULL,
 	"url_doi" varchar(255) NOT NULL,
-	"categoria" varchar(30) NOT NULL,
-	PRIMARY KEY ("id_publicacoes")
-);
-
-CREATE TABLE IF NOT EXISTS "categoria_noticias" (
-	"id_categoria_noticias" serial NOT NULL UNIQUE,
-	"categoria" varchar(30) NOT NULL,
-	PRIMARY KEY ("id_categoria_noticias")
-);
-
-CREATE TABLE IF NOT EXISTS "categoria_publicacoes" (
-	"id_categoria_publicacoes" serial NOT NULL UNIQUE,
-	"categoria" varchar(30) NOT NULL,
-	PRIMARY KEY ("id_categoria_publicacoes")
+	"id_categoria" integer NOT NULL,
+	CONSTRAINT "fk_categoria_publicacoes" FOREIGN KEY ("id_categoria") REFERENCES "categoria_publicacoes"("id_categoria_publicacoes") ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS "vagas" (
-	"id_vagas" serial NOT NULL UNIQUE,
+	"id_vagas" serial PRIMARY KEY,
 	"titulo" varchar(50) NOT NULL,
-	"descricao" varchar(255) NOT NULL,
-	"requisitos" varchar(255) NOT NULL,
-	"beneficios" varchar(255) NOT NULL,
+	"descricao" TEXT NOT NULL,
+	"requisitos" TEXT NOT NULL,
+	"beneficios" TEXT NOT NULL,
 	"ativo" boolean NOT NULL,
-	"data" timestamp with time zone NOT NULL,
-	PRIMARY KEY ("id_vagas")
+	"data" timestamp with time zone NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS "categoria_noticias" (
+	"id_categoria_noticias" serial PRIMARY KEY,
+	"nome" varchar(30) NOT NULL UNIQUE
+);
 
+CREATE TABLE IF NOT EXISTS "categoria_publicacoes" (
+	"id_categoria_publicacoes" serial PRIMARY KEY,
+	"nome" varchar(30) NOT NULL UNIQUE
+);
 
-ALTER TABLE "categoria_noticias" ADD CONSTRAINT "categoria_noticias_fk1" FOREIGN KEY ("categoria") REFERENCES "noticias"("categoria");
-ALTER TABLE "categoria_publicacoes" ADD CONSTRAINT "categoria_publicacoes_fk1" FOREIGN KEY ("categoria") REFERENCES "publicacoes"("categoria");
+select
