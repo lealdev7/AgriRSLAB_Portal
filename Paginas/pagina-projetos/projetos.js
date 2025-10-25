a616053d5db22c6f6765c191ee46d2a5e7679b8c
arianaferresan <afs3949@gmail.com>
2025-10-25 18:59:50 -0300

    Inclusao de novas imagens do projetos, refinamento responsividade

--- Paginas/pagina-projetos/projetos.js
+++ Paginas/pagina-projetos/projetos.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     var botoes = document.querySelectorAll(".botao-filtro");
     var itens = document.querySelectorAll(".item-galeria");
+    var mensagem = document.getElementById("mensagem");
 
     botoes.forEach(function (botao) {
         botao.addEventListener("click", function () {
@@ -12,16 +13,25 @@ document.addEventListener("DOMContentLoaded", function () {
             });
             botao.classList.add("ativo");
 
+            var itensVisiveis = false;
+
             // Filtra os itens
             itens.forEach(function (item) {
                 if (filtro === "todos" || item.classList.contains(filtro)) {
                     item.style.display = "block";
                     setTimeout(() => item.style.opacity = "1", 50);
+                    itensVisiveis = true;
                 } else {
                     item.style.opacity = "0";
                     setTimeout(() => item.style.display = "none", 400);
                 }
             });
+            if (itensVisiveis) {
+                mensagem.style.display ="none";
+            } else {
+                mensagem.style.display = "block"
+            }
         });
     });
 });
+
