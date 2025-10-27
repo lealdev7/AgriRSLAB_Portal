2923220f925c0ebc02eaa508d20adddcdce64542
arianaferresan <afs3949@gmail.com>
2025-10-27 00:26:59 -0300

    - Alinhamento das bordas da página membros com o padrão das demais paginas do site. - Ajuste no espaçamento entre os cards de membros e o footer. - Inclusão do botão Expandir/ Recolher de membros, com  JavaScript. - Limpeza e organização do código CSS da página Membros. - Criação da página de Notícia Individual. - Adição do botão Ler Mais nos cards de notícias.

--- Paginas/pagina-noticias/noticias.js
+++ Paginas/pagina-noticias/noticias.js
@@ -23,3 +23,26 @@ setaBaixo.addEventListener('click', () => {
 setaCima.addEventListener('click', () => {
   conteudoLinha.scrollBy({ top: -100, behavior: 'smooth' });
 });
+
+
+// Compartilhamento de notícias
+document.addEventListener("DOMContentLoaded", () => {
+  const url = encodeURIComponent(window.location.href);
+  const title = encodeURIComponent(document.title);
+
+  const shareWhatsApp = document.getElementById("shareWhatsApp");
+  const shareEmail = document.getElementById("shareEmail");
+  const shareLinkedIn = document.getElementById("shareLinkedIn");
+
+  if (shareWhatsApp) {
+    shareWhatsApp.href = `https://api.whatsapp.com/send?text=${title}%20${url}`;
+  }
+
+  if (shareEmail) {
+    shareEmail.href = `mailto:?subject=${title}&body=Veja%20essa%20matéria:%20${url}`;
+  }
+
+  if (shareLinkedIn) {
+    shareLinkedIn.href = `https://www.linkedin.com/sharing/share-offsite/?url=${url}`;
+  }
+});
